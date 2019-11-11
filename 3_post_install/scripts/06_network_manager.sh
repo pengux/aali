13b403e6becd9a87b5961b4b2c5eca24d3dc7018
Peter Nguyen <peter@mictis.com>
2019-11-11 13:48:59 +0100

    Fix typo

--- 3_post_install/scripts/06_network_manager.sh
+++ 3_post_install/scripts/06_network_manager.sh
@@ -4,6 +4,6 @@ echo "Installing Network Manager with VPN support"
 yay -S --noconfirm --needed networkmanager networkmanager-openvpn networkmanager-ssh-git
 echo
 
-echo "Enabling Networ Manager at Startup"
+echo "Enabling Network Manager at Startup"
 sudo systemctl enable NetworkManager.service
 echo
