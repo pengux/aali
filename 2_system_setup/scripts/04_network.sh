e30121c11b5e28867bc682c66dd6c8fcffeae9f0
Peter Nguyen <peter@mictis.com>
2019-11-11 11:55:57 +0100

    Fix bash var not interpreted in command

--- 2_system_setup/scripts/04_network.sh
+++ 2_system_setup/scripts/04_network.sh
@@ -9,7 +9,7 @@ echo
 echo -e "\nSetting host file\n"
 echo '127.0.0.1	localhost' >> /etc/hosts
 echo '::1		localhost' >> /etc/hosts
-echo '127.0.1.1	$HOSTNAME.localdomain	$HOSTNAME' >> /etc/hosts
+echo "127.0.1.1	$HOSTNAME.localdomain	$HOSTNAME" >> /etc/hosts
 echo
 
 echo -e "\nSetup and enable DHCP\n"
