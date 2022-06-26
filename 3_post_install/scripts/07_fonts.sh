10c0350b7f3a5b2fb9f9ac18fd9595c2d8b0d661
Peter Nguyen <peter@mictis.com>
2022-06-26 14:35:30 +0200

    add otf-font-awesome to installed fonts

--- 3_post_install/scripts/07_fonts.sh
+++ 3_post_install/scripts/07_fonts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Installing TTF fonts"
-yay -S terminus-font ttf-dejavu noto-fonts-cjk nerd-fonts-inconsolata
+yay -S terminus-font ttf-dejavu noto-fonts-cjk nerd-fonts-inconsolata otf-font-awesome
 echo
